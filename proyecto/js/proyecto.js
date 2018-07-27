9ddab2a4dd707cc5c5bd01da9ca449a4829ce4c1
NycollSM <nycollsotomorales21@gmail.com>
2018-07-26 23:36:29 -0600

    en la carpeta proyecto esta el html y css que se quiere fusionar con las carpetas de prueba y prueba 2

---
+++ proyecto/js/proyecto.js
@@ -0,0 +1,24 @@
+let formulario = document.getElementByName('formulario') [0];
+let elementos = formulario.elements;
+let boton = document.getElementById('comprar');
+
+/** let validar_id= function(){
+	if (formulario.direccion.value == 0){
+		alert("Por favor completar el espacio de ID");
+		e.preventDefault();
+	}
+}; 
+ let validar = function(e){
+ 	validar_id(e);
+ }
+ formulario.addEventListener('comprar' , validar);
+ **/
+
+ /** Validar Categoria s
+let categoria = function(){
+	if(formulario.categoria[].checked==true);
+	else{
+		alert('completar categoria');
+		e.preventDefault();
+	}
+}Validar Categoria**/
